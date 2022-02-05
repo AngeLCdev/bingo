e018a680c240d07eb8bb7a05b034a3fe9485539f
Ángel <akaishuichidc@outlook.com>
2022-02-05 11:50:52 +0100

    Actualizar rimas y los easter eggs

--- utils/getRima.js
+++ utils/getRima.js
@@ -1,23 +1,25 @@
 const getRima = ({ numero }) => {
     const rimas = [
-        { num: 1, rima: 'Con mi pene te vacuno' },
-        { num: 3, rima: 'Te la meto del revés' },
-        { num: 4, rima: 'Por tu culo mi aparato' },
+        { num: 1, rima: 'Que no pare ninguno' },
+        { num: 2, rima: 'Nos movemos los dos' },
+        { num: 3, rima: 'Lo mismo, pero al revés' },
+        { num: 4, rima: 'Me voy a la parra un rato' },
         { num: 5, rima: 'Por el culo te la hinco' },
-        { num: 6, rima: 'Si os agacháis, me la veis' },
-        { num: 8, rima: 'Por el culo te la entocho' },
-        { num: 9, rima: 'En tu culo se me mueve' },
+        { num: 6, rima: 'No me veis' },
+        { num: 7, rima: 'Que nadie se siente' },
+        { num: 8, rima: 'Me voy de bareta' },
+        { num: 9, rima: 'Esto se mueve' },
         { num: 13, rima: 'Agárramela que me crece' },
         { num: 15, rima: 'La niña bonita' },
         { num: 22, rima: 'Los dos patitos' },
-        { num: 25, rima: 'Por el culo te la hinco' },
+        /*{ num: 25, rima: 'Por el culo te la hinco' },
         { num: 35, rima: 'Por el culo te la hinco' },
         { num: 45, rima: 'Por el culo te la hinco' },
         { num: 55, rima: 'Doble Por el culo te la hinco' },
         { num: 65, rima: 'Por el culo te la hinco' },
         { num: 75, rima: 'Por el culo te la hinco' },
-        { num: 85, rima: 'Por el culo te la hinco' },
-        { num: 69, rima: 'El de abajo ni se mueve' },
+        { num: 85, rima: 'Por el culo te la hinco' },*/
+        //{ num: 69, rima: 'El de abajo ni se mueve' },
     ]
 
     const rimaNumeroArray = rimas.find(rima => rima.num == parseInt(numero))
