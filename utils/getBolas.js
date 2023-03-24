5c837247d13cd488e866e775f5834ef4426439ef
Ángel LC <angelcdev@outlook.es>
2023-03-24 19:19:50 +0100

    Amañar el sorteo del golf - Los de Alcázar vendimiaos

--- utils/getBolas.js
+++ utils/getBolas.js
@@ -2,9 +2,10 @@ export default function getBolas({ min, max }) {
     const arrayBolas = []
     let minArray = parseInt(min)
     let maxArray = parseInt(max)
+    let amanyoSorteo = [6, 10, 15, 26, 27, 35]
     for (let i = minArray; i <= maxArray; i++) {
 
-        arrayBolas.push(i < 10 ? `0${i}` : i)
+        !amanyoSorteo.includes(i) ? arrayBolas.push(i < 10 ? `0${i}` : i) : null
     }
 
     return arrayBolas
