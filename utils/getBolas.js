212a2eaf90382023717bf4a21d41a407cfd40046
Ángel LC <angelcdev@outlook.es>
2023-05-20 22:43:05 +0200

    Ya no están vendimiaos

--- utils/getBolas.js
+++ utils/getBolas.js
@@ -5,7 +5,8 @@ export default function getBolas({ min, max }) {
     let amanyoSorteo = [6, 10, 15, 26, 27, 35]
     for (let i = minArray; i <= maxArray; i++) {
 
-        !amanyoSorteo.includes(i) ? arrayBolas.push(i < 10 ? `0${i}` : i) : null
+        //!amanyoSorteo.includes(i) ? arrayBolas.push(i < 10 ? `0${i}` : i) : null
+        arrayBolas.push(i < 10 ? `0${i}` : i)
     }
 
     return arrayBolas
